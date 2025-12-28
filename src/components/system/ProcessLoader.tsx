c062d180a93cc6d295012e9c682cefab2bcac0e2
yuejt <yuethompson@gmail.com>
2025-12-28 15:45:19 -0500

    refactor ProcessLoader.tsx - refactor RenderProcess function - uses dynamic import for window component

--- src/components/system/ProcessLoader.tsx
+++ src/components/system/ProcessLoader.tsx
@@ -1,22 +1,29 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import type { ReactElement } from "react";
-import Window from "@/components/system/Window";
 import { useProcessContext } from "@/contexts/processes";
+import type { Process } from "@/types/contexts/process";
+
+const Window = dynamic(() => import("@/components/system/Window"));
+
+function RenderProcess({ Component, hasWindow }: Process): ReactElement {
+  return hasWindow ? (
+    <Window>
+      <Component />
+    </Window>
+  ) : (
+    <Component />
+  );
+}
 
 function ProcessLoader(): ReactElement {
   const { processes } = useProcessContext();
   return (
     <>
-      {Object.entries(processes).map(([id, { Component, hasWindow }]) =>
-        hasWindow ? (
-          <Window key={id}>
-            <Component key={id} />
-          </Window>
-        ) : (
-          <Component key={id} />
-        ),
-      )}
+      {Object.entries(processes).map(([id, process]) => (
+        <RenderProcess key={id} {...process} />
+      ))}
     </>
   );
 }

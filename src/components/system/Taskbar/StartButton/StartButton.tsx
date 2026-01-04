53d606808aa7690390ced2c774081995325dd78d
yuejt <yuethompson@gmail.com>
2026-01-03 19:50:20 -0500

    - styled for windows start button

--- src/components/system/Taskbar/StartButton/StartButton.tsx
+++ src/components/system/Taskbar/StartButton/StartButton.tsx
@@ -1,10 +1,11 @@
 import type { ReactElement } from "react";
 import styles from "./StartButton.module.scss";
+import StartButtonIcon from "./StartButtonIcon";
 
 export default function StartButton(): ReactElement {
   return (
-    <button className={styles.startButton} type="button">
-      Start
+    <button className={styles.startButton} type="button" aria-label="Start">
+      <StartButtonIcon />
     </button>
   );
 }

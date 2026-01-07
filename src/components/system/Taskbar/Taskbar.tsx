2b5affc5564bc6da7c1ef2ac206e2725e0ca7108
yuejt <yuethompson@gmail.com>
2026-01-07 17:12:13 -0500

    - theme toggle between light and dark

--- src/components/system/Taskbar/Taskbar.tsx
+++ src/components/system/Taskbar/Taskbar.tsx
@@ -3,12 +3,14 @@ import Clock from "./Clock/Clock";
 import StartButton from "./StartButton/StartButton";
 import styles from "./Taskbar.module.scss";
 import TaskbarEntries from "./TaskbarEntries/TaskbarEntries";
+import { ThemeToggle } from "./ThemeToggle/ThemeToggle";
 
 export default function Taskbar(): ReactElement {
   return (
     <nav className={styles.taskbar}>
       <StartButton />
       <TaskbarEntries />
+      <ThemeToggle />
       <Clock />
     </nav>
   );
